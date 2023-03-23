cadfaf7657761b38a62eec8537de01f1c803048a
rhemanth399 <120007414+rhemanth399@users.noreply.github.com>
2023-03-23 10:58:27 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,27 @@
 
 import React from "react";
 import './../styles/App.css';
+import {useState} from 'react';
 
 const App = () => {
+ 
+  const [selectedOption,setSelectedOption]=useState(null)
+  function handleButton(option){
+    setSelectedOption(option)
+  }
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="parent">
+      <div>
+        <h1>Child Component 1</h1>
+        <button onClick={()=>handleButton('option1')}>Option1</button>
+      </div>
+        <div>
+          <h1>Child Component 2</h1>
+          <button onClick={()=>handleButton('option2')}>Option2</button>
+        </div>
+        {selectedOption&&<p>Selected Option:{selectedOption}</p>}
     </div>
+
   )
 }
 
